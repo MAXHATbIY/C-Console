0c5a0d76c16fa9e272b941fd43a1f3bcced90693
Slava <mc_slava2@mail.ru>
2022-11-11 12:11:57 +0300

    add ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "слава")
+{
+    Console.WriteLine("Ура, это же Слава");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
